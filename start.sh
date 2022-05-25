fda8dae68f0108874c3d66c0d49a48206cac476c
KC Khais <khaiskc@gmail.com>
2022-05-25 18:16:24 +0530

    Initial commit

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/kckhais/Forward-Ajm2-Bot.git /Forward-Ajm2-Bot
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Forward-Ajm2-Bot
+fi
+cd /Forward-Ajm2-Bot
+pip3 install -U -r requirements.txt
+echo "Starting Bot This May Take Time Depending On Database Size..."
+python3 bot.py
